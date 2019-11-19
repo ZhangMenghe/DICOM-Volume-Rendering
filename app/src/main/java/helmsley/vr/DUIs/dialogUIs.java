fd9a1bc0a0b499638fa3ed313d1a2e207819ca8b
ZhangMenghe <zmh.leaves@gmail.com>
2019-11-19 12:42:42 -0800

    finish connection dialog

---
+++ app/src/main/java/helmsley/vr/DUIs/dialogUIs.java
@@ -0,0 +1,130 @@
+package helmsley.vr.DUIs;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import helmsley.vr.R;
+import helmsley.vr.proto.fileTransferClient;
+
+public class dialogUIs {
+    public static Activity activity;
+    final static String TAG = "dialogUIs";
+    private fileTransferClient downloader;
+    private TextView errText;
+    private Button sendButton;
+//    private ArrayList<datasetInfo> data_info_lst;
+    public dialogUIs(final Activity activity_){
+        activity = activity_;
+        SetupConnectDialog();
+
+    }
+    private void SetupConnectDialog(){
+        final AlertDialog.Builder layoutDialog_builder = new AlertDialog.Builder(activity);
+        final View dialogView = LayoutInflater.from(activity).inflate(R.layout.connect_dialog_layout, null);
+        //widgets
+        sendButton = (Button) dialogView.findViewById(R.id.connect_req_button);
+        EditText hostEdit = (EditText) dialogView.findViewById(R.id.host_edit_text);
+        hostEdit.setHint(activity.getString(R.string.DEFAULT_HOST));
+        EditText portEdit = (EditText) dialogView.findViewById(R.id.port_edit_text);
+        portEdit.setHint(activity.getString(R.string.DEFAULT_PORT));
+
+        errText = (TextView) dialogView.findViewById(R.id.grpc_err_msg);
+        errText.setMovementMethod(new ScrollingMovementMethod());
+
+
+        layoutDialog_builder.setTitle(activity.getString(R.string.dialog_connect_title));
+        layoutDialog_builder.setIcon(R.mipmap.ic_launcher_round);
+
+        layoutDialog_builder.setView(dialogView);
+        AlertDialog dialog = layoutDialog_builder.create();
+
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+//                        .hideSoftInputFromWindow(hostEdit.getWindowToken(), 0);
+                sendButton.setEnabled(false);
+                errText.setText("");
+                String host_addr = hostEdit.getText().toString();
+                host_addr = host_addr.isEmpty()?hostEdit.getHint().toString():host_addr;
+                String port_addr = portEdit.getText().toString();
+                port_addr = port_addr.isEmpty()?portEdit.getHint().toString():port_addr;
+
+                if(SetupDownloader(host_addr, port_addr)){
+                    Log.i(TAG, "====Connect to server successfully=====");
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        dialog.show();
+    }
+    private void SetupDownloadDialog(){
+        final AlertDialog.Builder layoutDialog_builder = new AlertDialog.Builder(activity);
+
+        final View dialogView = LayoutInflater.from(activity).inflate(R.layout.download_dialog_layout, null);
+
+        TextView dialogText = (TextView) dialogView.findViewById(R.id.dialog_text);
+        Button dialogBtnConfirm = (Button) dialogView.findViewById(R.id.dialog_btn_confirm);
+//        Button dialogBtnCancel = (Button) dialogView.findViewById(R.id.dialog_btn_cancel);
+
+        //recycle view
+
+        RecyclerView content_view = dialogView.findViewById(R.id.contentRecView);
+        content_view.setHasFixedSize(true);
+        //layout manager
+        RecyclerView.LayoutManager layout_manager = new LinearLayoutManager(activity);
+        content_view.setLayoutManager(layout_manager);
+        //adapter
+//        String[] debug_datas = {"d1","d2","d3"};
+//        content_view.setAdapter(new DialogAdapter(debug_datas));
+
+
+        layoutDialog_builder.setTitle(activity.getString(R.string.dialog_connect_title));
+        layoutDialog_builder.setIcon(R.mipmap.ic_launcher_round);
+
+        layoutDialog_builder.setView(dialogView);
+
+        dialogText.setText("我是自定义layout的弹窗！！");
+
+        AlertDialog dialog = layoutDialog_builder.create();
+        dialogBtnConfirm .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SetupDownloader("137.110.112.178","23333");
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+
+    }
+
+    private boolean SetupDownloader(String host, String port){
+        downloader = new fileTransferClient();//new fileTransferClient(host, port);
+
+        String res_msg = downloader.Setup(host, port);
+        if(res_msg.equals(""))
+            return true;
+
+        errText.setText(res_msg);
+        errText.setVisibility(View.VISIBLE);
+        sendButton.setEnabled(true);
+        return false;
+    }
+}
