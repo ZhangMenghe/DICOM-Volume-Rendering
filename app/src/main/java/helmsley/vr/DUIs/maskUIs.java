333a92c6a30501042de606021f9b31786513ae77
zhangmenghe <zmh.leaves@gmail.com>
2020-08-18 17:52:44 -0700

    save states

--- app/src/main/java/helmsley/vr/DUIs/maskUIs.java
+++ app/src/main/java/helmsley/vr/DUIs/maskUIs.java
@@ -1,6 +1,9 @@
 package helmsley.vr.DUIs;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,12 +11,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.common.primitives.Booleans;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 import helmsley.vr.R;
 
@@ -21,6 +28,7 @@ public class maskUIs extends BasePanel{
     private RecyclerView recyclerView;
     private maskRecyclerViewAdapter recyclerViewAdapter;
     private CheckBox recolor_cb;
+    private maskCheckboxListAdapter cbAdapter_;
 
     public maskUIs(final Activity activity, ViewGroup parent_view) {
         super(activity, parent_view);
@@ -38,6 +46,11 @@ public class maskUIs extends BasePanel{
         recyclerViewAdapter = new maskRecyclerViewAdapter(activity, recyclerView);
         recyclerView.setAdapter(recyclerViewAdapter);
 
+        //setup spinner
+        Spinner spinner_check = (Spinner)panel_.findViewById(R.id.spinner_check_mask_control);
+        cbAdapter_ = new maskCheckboxListAdapter(activity);
+        spinner_check.setAdapter(cbAdapter_);
+
         recolor_cb = (CheckBox)panel_.findViewById(R.id.check_mask_color);
         String[] narr = activity.getResources().getStringArray(R.array.mask_check_params);
         String[] varr = activity.getResources().getStringArray(R.array.mask_check_values);
@@ -49,25 +62,25 @@ public class maskUIs extends BasePanel{
                 JUIInterface.JUIsetChecks(narr[1], isChecked);
             }
         });
-        setup_checks(
-                panel_,
-                R.array.mask_check_params, R.array.mask_check_values,
-                R.id.check_mask_show, 0);
+        setup_checks(R.array.mask_check_params, R.array.mask_check_values);
     }
     @Override
     public void Reset(){
         recyclerViewAdapter.Reset();
-        primary_checkbox.setChecked(default_primary_check);
+//        primary_checkbox.setChecked(default_primary_check);
     }
     public void ResetWithTemplate(LinkedHashMap map, ArrayList<String> names, ArrayList<Boolean> values){
         LinkedHashMap maskmap = (LinkedHashMap) map.getOrDefault("mask", null);
         if(maskmap == null) return;
         boolean status = (Boolean) maskmap.getOrDefault("status", default_primary_check);
-        primary_checkbox.setChecked(status);
+//        primary_checkbox.setChecked(status);
+        cbAdapter_.setValue(0, status);
         values.add(status);
 
         boolean recolor = (Boolean) maskmap.getOrDefault("recolor", default_primary_check);
-        recolor_cb.setChecked(recolor);
+        cbAdapter_.setValue(1, status);
+
+//        recolor_cb.setChecked(recolor);
         values.add(recolor);
 
         recyclerViewAdapter.Reset(Booleans.toArray((ArrayList<Boolean>)maskmap.getOrDefault("value", null)));
@@ -75,8 +88,8 @@ public class maskUIs extends BasePanel{
     }
     public LinkedHashMap getCurrentStates(){
         LinkedHashMap map = new LinkedHashMap();
-        map.put("status", primary_checkbox.isChecked());
-        map.put("recolor", recolor_cb.isChecked());
+        map.put("status", cbAdapter_.getValue(0));//primary_checkbox.isChecked());
+        map.put("recolor", cbAdapter_.getValue(1));//recolor_cb.isChecked());
         ArrayList<Boolean> values= new ArrayList<Boolean>();
         for(boolean b:recyclerViewAdapter.getValues()) values.add(b);
         map.put("value", values);
@@ -86,4 +99,64 @@ public class maskUIs extends BasePanel{
     public void showHidePanel(boolean isPanelOn){
         super.showHidePanel(isPanelOn);
     }
+    private static class maskCheckboxListAdapter extends ListAdapter {
+        List<Boolean> item_values;
+        maskCheckboxListAdapter(Context context) {
+            super(context, context.getString(R.string.mask_check_name));
+            //setup values
+            Resources res = context.getResources();
+            item_names = Arrays.asList(res.getStringArray(R.array.mask_check_params));
+            TypedArray check_values = res.obtainTypedArray(R.array.mask_check_values);
+            item_values = new ArrayList<>();
+            for (int i = 0; i < item_names.size(); i++) item_values.add(check_values.getBoolean(i, false));
+            check_values.recycle();
+        }
+        void setValue(int id, boolean value){
+            if(id < item_values.size() && item_values.get(id)!=value) {
+                item_values.set(id, value);
+                notifyDataSetChanged();
+            }
+        }
+        boolean getValue(int id){return (id<item_values.size())?item_values.get(id):false;}
+        public View getDropDownView(int position, View convertView, ViewGroup parent){
+            ViewContentHolder holder;
+            if (convertView == null) {
+                holder = new ViewContentHolder();
+                convertView = mInflater.inflate(R.layout.spinner_check_layout, null);
+                holder.text_name = (TextView) convertView.findViewById(R.id.checkName);
+                holder.checkBox = (CheckBox) convertView.findViewById(R.id.checkCheckBox);
+
+                convertView.setTag(R.layout.spinner_check_layout, holder);
+
+            } else {
+                holder = (ViewContentHolder) convertView.getTag(R.layout.spinner_check_layout);
+            }
+            holder.text_name.setText(item_names.get(position));
+            holder.checkBox.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener(){
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView,
+                                             boolean isChecked) {
+                    if(item_values.get(position) == isChecked) return;
+                    item_values.set(position, isChecked);
+                    JUIInterface.JUIsetChecks(item_names.get(position), isChecked);
+
+//                    int relpos = 1-position;
+//                    if(isChecked && item_values.get(relpos)){
+//                        //update another one
+//                        JUIInterface.JUIsetChecks(item_names.get(relpos), false);
+//                        item_values.set(relpos, false);
+//                        notifyDataSetChanged();
+//                    }
+                }
+            });
+            holder.checkBox.setTag(position);
+            holder.checkBox.setChecked(item_values.get(position));
+            return convertView;
+        }
+        class ViewContentHolder{
+            TextView text_name;
+            CheckBox checkBox;
+        }
+    }
+
 }
--- volume-rendering-cpp/vrController.cpp
+++ volume-rendering-cpp/vrController.cpp
@@ -194,19 +194,19 @@ void vrController::onDraw() {
 
     if(volume_model_dirty){updateVolumeModelMat();volume_model_dirty = false;}
 //    if(Manager::param_bool[dvr::CHECK_DRAW_POLYGON]){
-        meshRenderer_->Draw();
+//        meshRenderer_->Draw();
         // for(auto mrenderer_:mesh_renders)
         //     if(mrenderer_!=nullptr) mrenderer_->Draw();
 //    }
 
 //    if(Manager::param_bool[dvr::CHECK_DRAW_VOLUME]){
-//        precompute();
-//        if(isRayCasting())  raycastRenderer_->Draw();
-//        else texvrRenderer_->Draw();
+        precompute();
+        if(isRayCasting())  raycastRenderer_->Draw();
+        else texvrRenderer_->Draw();
 //    }
 //    //draw centerline
-    for(auto line:line_renderers_)
-        line.second->onDraw(ModelMat_ * raycastRenderer_->getDimScaleMat());
+//    for(auto line:line_renderers_)
+//        line.second->onDraw(ModelMat_ * raycastRenderer_->getDimScaleMat());
 }
 void vrController::onTouchMove(float x, float y) {
     if(!tex_volume) return;
