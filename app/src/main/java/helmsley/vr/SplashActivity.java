6de69d4ccb7797178db051086efef5b2e82ad31a
zhangmenghe <zmh.leaves@gmail.com>
2020-09-21 16:15:53 -0700

    add splash scren

---
+++ app/src/main/java/helmsley/vr/SplashActivity.java
@@ -0,0 +1,21 @@
+package helmsley.vr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // yourMethod();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);   //2 seconds
+    }
+}
